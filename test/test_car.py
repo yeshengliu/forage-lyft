c8322a035be7a7cfe9129cdf1975486fcaf8b631
Yesheng Liu <yesheng.liu@outlook.com>
2023-03-20 18:12:53 -0700

    Update test cases for spindler battery

--- test/test_car.py
+++ test/test_car.py
@@ -7,7 +7,7 @@ from car_factory import CarFactory
 class TestCalliope(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 4)
         current_mileage = 0
         last_service_mileage = 0
 
@@ -45,7 +45,7 @@ class TestCalliope(unittest.TestCase):
 class TestGlissade(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 4)
         current_mileage = 0
         last_service_mileage = 0
 
