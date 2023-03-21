0eb52841af9f2abc3acfac1be7d77d530d8f6384
Yesheng Liu <yesheng.liu@outlook.com>
2023-03-20 18:07:09 -0700

    Change the service period of Spindler battery from 2 to 3 years

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -10,4 +10,4 @@ class SpindlerBattery(Battery):
         self.current_date = current_date
 
     def needs_service(self):
-        return self.current_date > self.last_service_date.replace(year=self.last_service_date.year + 2)
+        return self.current_date > self.last_service_date.replace(year=self.last_service_date.year + 3)
